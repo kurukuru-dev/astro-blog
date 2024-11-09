20af427fa09055b01d8c422517d3189fdfc4ffb6
kotarokomori <komopro.works@gmail.com>
2024-11-09 15:47:14 +0900

    画像のパスには絶対パスを指定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
-// https://astro.build/config
+const isGitHubPages = process.env.GITHUB_PAGES === "true";
+
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: "https://kurukuru-dev.github.io",
-  base: process.env.GITHUB_PAGES ? "/astro-ec-site/" : "/",
+  site: "https://kurukuru-dev.github.io/astro-ec-site/",
+  base: isGitHubPages ? "/astro-ec-site/" : "/",
 });
