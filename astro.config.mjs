b8543cf09aacadd0a9742672a130da96228e1f91
kotarokomori <komopro.works@gmail.com>
2024-11-09 15:53:00 +0900

    パスを修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,8 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-const isGitHubPages = process.env.GITHUB_PAGES === "true";
-
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: "https://kurukuru-dev.github.io/astro-ec-site/",
-  base: isGitHubPages ? "/astro-ec-site/" : "/",
+  site: "https://kurukuru-dev.github.io",
+  base: process.env.GITHUB_PAGES ? "/astro-ec-site/" : "/",
 });
