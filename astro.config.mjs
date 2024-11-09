0b243139ee40739eb2f4e9c1477d85042a15dab6
kotarokomori <komopro.works@gmail.com>
2024-11-09 16:04:04 +0900

    画像パスの解決

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 export default defineConfig({
+  output: "static",
   integrations: [tailwind(), react()],
   site: "https://kurukuru-dev.github.io",
   base: process.env.GITHUB_PAGES ? "/astro-ec-site/" : "/",
